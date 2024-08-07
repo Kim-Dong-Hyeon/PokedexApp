3341297819f351d4cf3a8f91f9efd013bc57510c
Kim-Dong-Hyeon <dhkim960206@gmail.com>
2024-08-08 05:46:22 +0900

    Feat: Level 2 구현(MVVM - Model 구현)

--- PokedexApp/Model/PokemonDetail.swift
+++ PokedexApp/Model/PokemonDetail.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+// 포켓몬의 디테일 정보를 처리하기 위한 구조체
+struct PokemonDetail: Codable {
+  let id: Int?
+  let name: String?
+  let types: [PokemonTypeSlot]?
+  let height: Int?
+  let weight: Int?
+}
+
+struct PokemonTypeSlot: Codable {
+  let type: PokemonType
+}
+
+struct PokemonType: Codable {
+  let name: String
+}
