f2ef800c98630e6e6476908610a7fb108d2f35fa
Kim-Dong-Hyeon <dhkim960206@gmail.com>
2024-08-08 16:39:59 +0900

    Feat: MVVM - MainViewController, PokemonCell 구현

--- PokedexApp/App/AppDelegate.swift
+++ PokedexApp/App/AppDelegate.swift
@@ -15,7 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     
     let window = UIWindow(frame: UIScreen.main.bounds)
-    window.rootViewController = UINavigationController(rootViewController: MainViewController())
+//    window.rootViewController = UINavigationController(rootViewController: MainViewController())
+    window.rootViewController = MainViewController()
     window.makeKeyAndVisible()
     
     self.window = window
---
+++ PokedexApp/View/PokemonCell.swift
@@ -0,0 +1,52 @@
+//
+//  PokemonCell.swift
+//  PokedexApp
+//
+//  Created by 김동현 on 8/8/24.
+//
+
+import UIKit
+import SnapKit
+
+class PokemonCell: UICollectionViewCell {
+  static let id = "PokemonCell"
+  
+  let imageView: UIImageView = {
+    let imageView = UIImageView()
+    imageView.contentMode = .scaleAspectFill
+    imageView.clipsToBounds = true
+    imageView.backgroundColor = .cellBackground
+    imageView.layer.cornerRadius = 10
+    return imageView
+  }()
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    contentView.addSubview(imageView)
+    imageView.frame = contentView.bounds
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  func configure(with pokemon: PokemonListItem) {
+    if let urlString = pokemon.url, let id = getIdFromUrl(urlString) {
+      let imageUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/\(id).png"
+      if let url = URL(string: imageUrl) {
+        DispatchQueue.global().async {
+          if let data = try? Data(contentsOf: url), let image = UIImage(data: data) {
+            DispatchQueue.main.async {
+              self.imageView.image = image
+            }
+          }
+        }
+      }
+    }
+  }
+  
+  private func getIdFromUrl(_ url: String) -> String? {
+    let components = url.split(separator: "/")
+    return components.last.map { String($0) }
+  }
+}
